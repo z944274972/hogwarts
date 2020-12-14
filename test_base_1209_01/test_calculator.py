d25f30fef016612aa36e56b4935809cd0de80c1f
z944274972 <944274972@qq.com>
2020-12-14 18:45:40 +0800

    可以用yaml 进行数据驱动 结合allure 报告

--- test_base_1209_01/test_calculator.py
+++ test_base_1209_01/test_calculator.py
@@ -6,8 +6,8 @@
 
 import pytest
 import yaml
-import allure
 import os
+import allure
 from test_base_1209_01.pythoncode.calculator import  Calculator
 
 def get_datas(path,data=None,ids=None):
@@ -32,28 +32,28 @@ class TestCalculator:
 
     @allure.feature("加法")
     @allure.title("加法case")
-    @pytest.mark.parametrize("a,b,expected", get_datas("yaml/cal.yml","add")[0],ids=get_datas("yaml/cal.yml",ids="myid")[1])
+    @pytest.mark.parametrize("a,b,expected", get_datas("yaml/cal.yml","add")[0],ids=get_datas("yaml/cal.yml",ids="myid_add")[1])
     def test_add(self,a,b,expected,myfixture):
         with allure.step("进行加法计算"):
             assert expected ==myfixture.add(a,b)
 
     @allure.feature("减法")
     @allure.title("减法case")
-    @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("yaml/cal.yml"))["jian"])
+    @pytest.mark.parametrize("a,b,expected", get_datas("yaml/cal.yml","jian")[0],ids=get_datas("yaml/cal.yml",ids="myid_jian")[1])
     def test_jia(self, a, b, expected,myfixture):
         with allure.step("进行减法计算"):
             assert expected == myfixture.jian(a, b)
 
     @allure.feature("乘法")
     @allure.title("乘法case")
-    @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("yaml/cal.yml"))["cheng"])
+    @pytest.mark.parametrize("a,b,expected", get_datas("yaml/cal.yml","cheng")[0],ids=get_datas("yaml/cal.yml",ids="myid_cheng")[1])
     def test_cheng(self,a,b,expected,myfixture):
         with allure.step("进行乘法计算"):
             assert expected ==myfixture.cheng(a,b)
 
     @allure.feature("除法")
     @allure.title("除法case")
-    @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("yaml/cal.yml"))["chu"])
+    @pytest.mark.parametrize("a,b,expected", get_datas("yaml/cal.yml","chu")[0],ids=get_datas("yaml/cal.yml",ids="myid_chu")[1])
     def test_chu(self,a,b,expected,myfixture):
         with allure.step("进行除法计算"):
             assert expected ==myfixture.chu(a,b)
