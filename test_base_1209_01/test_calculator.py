47d98f6577b85d8923d0fa599152feca886606c0
“xe <x944274972@qq.com>
2020-12-14 18:05:43 +0800

    更改

--- test_base_1209_01/test_calculator.py
+++ test_base_1209_01/test_calculator.py
@@ -33,33 +33,33 @@ class TestCalculator:
     def teardown_class(self):
         print("计算结束")
 
-    @allure.feature("加法")
-    @allure.title("加法case")
-    @pytest.mark.parametrize("a,b,expected",yaml.safe_load(open("./yaml/cal.yal"))["add"])
-    def test_add(self,a,b,expected):
-        with allure.step("进行加法计算"):
-            assert expected ==self.cal.add(a,b)
-
-    @allure.feature("减法")
-    @allure.title("减法case")
-    @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("./yaml/cal.yal"))["jian"])
-    def test_jia(self, a, b, expected):
-        with allure.step("进行减法计算"):
-            assert expected == self.cal.jian(a, b)
-
-    @allure.feature("乘法")
-    @allure.title("乘法case")
-    @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("./yaml/cal.yal"))["cheng"])
-    def test_cheng(self,a,b,expected):
-        with allure.step("进行乘法计算"):
-            assert expected ==self.cal.cheng(a,b)
-
-    @allure.feature("除法")
-    @allure.title("除法case")
-    @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("./yaml/cal.yal"))["chu"])
-    def test_chu(self,a,b,expected):
-        with allure.step("进行除法计算"):
-            assert expected ==self.cal.chu(a,b)
+    # @allure.feature("加法")
+    # @allure.title("加法case")
+    # @pytest.mark.parametrize("a,b,expected",yaml.safe_load(open("./yaml/cal.yal"))["add"])
+    # def test_add(self,a,b,expected):
+    #     with allure.step("进行加法计算"):
+    #         assert expected ==self.cal.add(a,b)
+    #
+    # @allure.feature("减法")
+    # @allure.title("减法case")
+    # @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("./yaml/cal.yal"))["jian"])
+    # def test_jia(self, a, b, expected):
+    #     with allure.step("进行减法计算"):
+    #         assert expected == self.cal.jian(a, b)
+    #
+    # @allure.feature("乘法")
+    # @allure.title("乘法case")
+    # @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("./yaml/cal.yal"))["cheng"])
+    # def test_cheng(self,a,b,expected):
+    #     with allure.step("进行乘法计算"):
+    #         assert expected ==self.cal.cheng(a,b)
+    #
+    # @allure.feature("除法")
+    # @allure.title("除法case")
+    # @pytest.mark.parametrize("a,b,expected", yaml.safe_load(open("./yaml/cal.yal"))["chu"])
+    # def test_chu(self,a,b,expected):
+    #     with allure.step("进行除法计算"):
+    #         assert expected ==self.cal.chu(a,b)
 
     @pytest.mark.parametrize("a,b,expected", get_datas("yaml/cal.yml","add")[0],ids=get_datas("yaml/cal.yml",ids="myid")[1])
     def test_add(self,a,b,expected,myfixture):
